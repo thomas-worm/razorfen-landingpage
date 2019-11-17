53a0311a1bf6520e82255a798c02e505be54b504
thomas-worm <thomas.worm@thomas-worm.de>
2019-11-17 15:43:10 +0000

    Edited index.php

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
         <title>World of Warcraft Classic - Realm Razorfen</title>
     </head>
     <body>
-        <h1Y>Hello World!</h1Y>
+        <h1>World of Warcraft Classic - EU - Razorfen</h1>
+        <h2>Raids</h2>
+        <ul>
+            <li>
+                <h3>Just4Fun</h3>
+                <p>Erster Raidtag: Donnerstags ab 19:00 Uhr</p>
+                <p>Zweiter Raidtag: sofern nötig, flexibel über Gruppe ausgemacht</p>
+                <p>Interesse? <a href="https://blizzard.com/invite/x0vAVqSaze" />Dann trete der Battle.net-Gruppe bei!</a></p>
+            </li>
+        </ul>
     </body>
 </html>
