c6c98a3ccad6b9756574b063e9fa557fa13541ad
thomas-worm <thomas.worm@thomas-worm.de>
2019-11-17 15:56:53 +0000

    Added welcome text.

--- index.php
+++ index.php
@@ -20,15 +20,19 @@
                 </ul>
             </div>
         </nav>
-        <h2>Raids</h2>
-        <ul>
-            <li>
-                <h3>Just4Fun</h3>
-                <p>Erster Raidtag: Donnerstags ab 19:00 Uhr</p>
-                <p>Zweiter Raidtag: sofern nötig, flexibel über Gruppe ausgemacht</p>
-                <p>Interesse? <a href="https://blizzard.com/invite/x0vAVqSaze" />Dann trete der Battle.net-Gruppe bei!</a></p>
-            </li>
-        </ul>
+        <div class="container">
+            <h1>Herzlich Willkommen</h1>
+            <p>... auf der &Uuml;bersichtsseite des World of Warcraft Classic Realms Razorfen (EU).
+            <h2>Raids</h2>
+            <ul>
+                <li>
+                    <h3>Just4Fun</h3>
+                    <p>Erster Raidtag: Donnerstags ab 19:00 Uhr</p>
+                    <p>Zweiter Raidtag: sofern nötig, flexibel über Gruppe ausgemacht</p>
+                    <p>Interesse? <a href="https://blizzard.com/invite/x0vAVqSaze" />Dann trete der Battle.net-Gruppe bei!</a></p>
+                </li>
+            </ul>
+        </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
